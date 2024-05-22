24a4ad36b9c05242aff149c85885f5d983801f83
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-05-22 16:49:51 +0600

    Configuration Done

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# CRUD-with-JWT-Access-Authentication
-Role based CRUD operation
+# Create
+1. Create a virtual environment with python 3.10.12
+2. Create .env with the same configuration of settings and given database requirements name
+
+# Activation
+1. Activate your virtual environment
+
+# Run
+1. pip install -r requirements.txt
+2. python manage.py runserver
---
+++ taskApp/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenVerifyView
+from taskApp import views
+
+#from rest_framework_simplejwt.views import (
+#    TokenRefreshView,
+#)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+#router = DefaultRouter()
+#router.register(r'todoitems', ToDoTaskAPIView,basename='todoitem')
+
+urlpatterns = [
+
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+
+]
